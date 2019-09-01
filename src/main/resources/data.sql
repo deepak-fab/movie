4bc9ef5884654cae98158ced085492b8f17853e4
Deepak Mittal <deepak.mittal@ad.infosys.com>
2019-09-01 12:45:25 +0530

    h2 db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,7 @@ insert into theatre (theatre_id, theatre_name, city_id, movie_id) values
 (2, 'Mall of travancore' , 1,1001) , 
 (3, 'Centrum mall', 1, 1002);
 
-insert into show (theatre_id, show_date_time, total_seats, available_seats) values
+insert into showdtl (theatre_id, show_date_time, total_seats, available_seats) values
 (1, '2018-12-26 11:30:00.0000', 100, 100 ),
 (1, '2018-12-26 16:30:00.0000', 100, 100 ),
 (2, '2018-12-26 11:30:00.0000', 100, 100 ),
